2d80478e19db1148d6743798e1a4a9f8445aee26
hassanhashmi77 <66280195+hassanhashmi77@users.noreply.github.com>
2020-09-09 22:13:25 +0500

    new

---
+++ src/MnavBar.js
@@ -0,0 +1,29 @@
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css';
+
+function MnavB() {
+
+
+    return (
+        <div >
+            <nav>
+                <div class="nav-wrapper">
+                    <a href="#" class="brand-logo">Logo</a>
+                    <ul id="nav-mobile" class="right hide-on-med-and-down">
+                        <li><a href="sass.html">Sass</a></li>
+                        <li><a href="badges.html">Components</a></li>
+                        <li><a href="collapsible.html">JavaScript</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    );
+}
+componentDidMount(){
+    document.addEventListener('DOMContentLoaded', function () {
+        var elems = document.querySelectorAll('.sidenav');
+        var instances = M.Sidenav.init(elems, options);
+    });
+}
+export default MnavB;
+
---
+++ src/pages/Footer.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+class Footer extends Component {
+    render() {
+        return (
+            <div >
+                <footer id="footer">
+                    <ul class="icons">
+                        <li><a href="#" class="icon brands alt fa-twitter"><span class="label">Twitter</span></a></li>
+                        <li><a href="#" class="icon brands alt fa-facebook-f"><span class="label">Facebook</span></a></li>
+                        <li><a href="#" class="icon brands alt fa-linkedin-in"><span class="label">LinkedIn</span></a></li>
+                        <li><a href="#" class="icon brands alt fa-instagram"><span class="label">Instagram</span></a></li>
+                        <li><a href="#" class="icon brands alt fa-github"><span class="label">GitHub</span></a></li>
+                        <li><a href="#" class="icon solid alt fa-envelope"><span class="label">Email</span></a></li>
+                    </ul>
+                    <ul class="copyright">
+                        <li>&copy; All rights reserved.</li>
+                    </ul>
+                </footer>
+                <script src="assets/js/jquery.min.js"></script>
+                <script src="assets/js/jquery.scrolly.min.js"></script>
+                <script src="assets/js/jquery.dropotron.min.js"></script>
+                <script src="assets/js/jquery.scrollex.min.js"></script>
+                <script src="assets/js/browser.min.js"></script>
+                <script src="assets/js/breakpoints.min.js"></script>
+                <script src="assets/js/util.js"></script>
+                <script src="assets/js/main.js"></script>
+
+            </div>
+        );
+    }
+
+}
+
+export default Footer;
+
+
